0ebe9b5d324c7025e80adc5ed7ac6191005caf63
Nikolay <120409996+nikolikovalev@users.noreply.github.com>
2023-04-06 21:34:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 411809593 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     x = np.log(x - 163)
